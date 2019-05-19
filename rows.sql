5fad8b0ee573d85937910ad98f5703a9f6beca79
Kamoliddin <Kamoliddin7usmonov@gmail.com>
2019-05-19 15:57:58 +0900

    Flask with SQL

---
+++ rows.sql
@@ -0,0 +1,6 @@
+insert into flight (origin, destination, duration) values ('London', '', );
+insert into flight (origin, destination, duration) values ('Shanghai', 'Parij', );
+insert into flight (origin, destination, duration) values ('', '', );
+insert into flight (origin, destination, duration) values ('', '', );
+insert into flight (origin, destination, duration) values ('', '', );
+
